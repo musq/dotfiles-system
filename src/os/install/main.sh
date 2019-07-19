e81fada1490cc1b21211c6e3e4314451e48eff65
Ashish Ranjan <ashish72@pm.me>
2019-07-20 00:30:44 +0530

    feat(setup): Allow non-interactive Nix install
    When -y argument is passed to setup.sh, the Nix installer will be
    run non-interactively. So, it won't ask y/n questions from the user.

--- src/os/install/main.sh
+++ src/os/install/main.sh
@@ -8,6 +8,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n ● Installs\n"
 
 
+skipQuestions=false
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+skip_questions "$1" \
+    && skipQuestions=true
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 OS="$(get_os)"
@@ -21,8 +28,23 @@ fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if ! cmd_exists "nix" "sudo"; then
-    sh <(curl https://nixos.org/nix/install) --daemon
+
+    print_in_purple "\n   Install Nix package manager\n\n"
+
+    if "$skipQuestions"; then
+
+        # Run installer non-interactively
+        # https://www.igorkromin.net/index.php/2017/04/12/invoking-interactive-shell-scripts-non-interactively
+
+        echo | sh <(curl https://nixos.org/nix/install) --daemon
+
+    else
+        sh <(curl https://nixos.org/nix/install) --daemon
+    fi
+
 fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if cmd_exists "nix" "sudo"; then
     ./nix/main.sh "$@"
