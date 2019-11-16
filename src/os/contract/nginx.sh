2cd19b346cbf985cd6b84b6a8cb21051ae842804
Ashish Ranjan <ashish@tug.ro>
2019-11-16 09:32:14 +0000

    fix(nginx): Remove www-data group creation

--- src/os/contract/nginx.sh
+++ src/os/contract/nginx.sh
@@ -8,8 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   NGINX\n\n"
 
-add_group "www-data" "system"
 add_user "nginx" "NGINX" "system"
-add_user_to_group "www-data" "nginx"
 
-harden "/var/log/nginx" "nginx" "www-data" 640 750
+harden "/var/log/nginx" "nginx" "nginx" 640 750
