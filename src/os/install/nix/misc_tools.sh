e5a372cf657b4347a0e961141adcbf5698d4e959
Ashish Ranjan <ashish@tug.ro>
2019-11-18 13:37:55 +0000

    feat(duplicati): Install on Nix

--- src/os/install/nix/misc_tools.sh
+++ src/os/install/nix/misc_tools.sh
@@ -8,6 +8,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Miscellaneous Tools\n\n"
 
+nix_install "Duplicati" "nixpkgs.duplicati"
 nix_install "htop" "nixpkgs.htop"
 nix_install "Icdiff" "nixpkgs.icdiff" "$@"
 nix_install "Tmux" "nixpkgs.tmux" "$@"
